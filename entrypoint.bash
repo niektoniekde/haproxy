91036ff1eb79ee1efdeeea3d559ecc2cd3df379d
Jaroslav Petras <niekto@niekde.sk>
2023-12-19 10:53:19 +0100

    enable quiet mode

--- entrypoint.bash
+++ entrypoint.bash
@@ -1,10 +1,15 @@
 #! /bin/bash
-set -euE
-CONFD="/usr/local/etc/haproxy/conf.d"
+set -euxE
+LETCD="/usr/local/etc/haproxy"
+CONFD="${LETCD}/conf.d"
+DATAD="/srv/data/haproxy"
 
 if ! [[ -n $(ls -1A ${CONFD}) ]]; then
   echo "ERROR: there are no configuration files to be included in directory: '${CONFD}' " >&2
   exit 100
 fi
 
-exec /usr/sbin/haproxy -W -db -f "/usr/local/etc/haproxy/haproxy.cfg" -f "${CONFD}"
+if ! [[ -d ${DATAD} ]]; then mkdir -p "${DATAD}"; fi
+chmod ug=rwX,o-rX -R "${DATAD}"
+
+exec /usr/sbin/haproxy -W -db -q -f "/usr/local/etc/haproxy/haproxy.cfg" -f "${CONFD}"
